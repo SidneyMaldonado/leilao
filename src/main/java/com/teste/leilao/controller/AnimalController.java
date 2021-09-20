5a2ac73b9a6788515c1d8000e5ecb02a25243073
Guilherme F. Ferreira <gferreira@itelinfo.com>
2021-09-20 16:57:10 -0400

    Att

--- src/main/java/com/teste/leilao/controller/AnimalController.java
+++ src/main/java/com/teste/leilao/controller/AnimalController.java
@@ -1,5 +1,55 @@
 package com.teste.leilao.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.teste.leilao.entities.Animal;
+import com.teste.leilao.repositories.AnimalRepository;
+
+
+
+@RestController
+@RequestMapping("animal")
 public class AnimalController {
 
+	@Autowired
+	private AnimalRepository animalRepositorio;
+	
+	@GetMapping("listar")
+	public List<Animal> listar() {
+	
+		List<Animal> lista = animalRepositorio.findAll(); 
+		return lista;
+	}
+	
+	@PostMapping("incluir")	//rota
+	public Mensagem salvar(@RequestBody Animal animal ) {
+		
+		animal.setIdAnimal(0);	
+		AnimalBiz animalBiz = new animalBiz();
+		
+		try {
+			//validar todos os dominios e regras
+		if (animalBiz.Validade(animal)) {
+			this.animalRepositorio.save(animal);
+			this.animalRepositorio.flush();
+		} else {
+			return animalBiz.msg; 	// retorna a mensagem de erro
+		}
+	} catch (Exception e) {
+		animalBiz.msg.mensagens.add(e.getMessage());
+		return animalBiz.msg;
+	}
+	animalBiz.msg.mensagens.add("OK");
+		
+	return animalBiz.msg;
+		
+	}
 }
+
